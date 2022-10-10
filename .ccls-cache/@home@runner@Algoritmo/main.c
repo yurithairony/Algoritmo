2806f403da9569f89e4bf425e544235cf82ae4b5
yurithairony <102562875+yurithairony@users.noreply.github.com>
2022-10-10 11:59:15 +0000

    resolução da questao 9

--- .ccls-cache/@home@runner@Algoritmo/main.c
+++ .ccls-cache/@home@runner@Algoritmo/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-  int n, i;
-  scanf("%d", &n);
-  for (i=1; i<n; i=i+1){
-    if (n % i == 0) {
-      printf("%d ", i);
+  int x, y, i, mdc;
+  scanf("%d %d", &x, &y);
+  for (i=1; i<= x && i<= y; i = i + 1){
+    if (x % i == 0 && y % i == 0) {
+        mdc = i;
     }
   }
-  printf("%d\n", n);
+  printf("%d\n", mdc);
 }
--- Listas/Lista1/q09.c
+++ Listas/Lista1/q09.c
@@ -3,10 +3,10 @@
 int main(void) {
   int x, y, i, mdc;
   scanf("%d %d", &x, &y);
-  for (i=1; i<= x && i<= y; i = i + 1){
+  for (i=0; i<= x && i<= y; i = i + 1){
     if (x % i == 0 && y % i == 0) {
         mdc = i;
     }
   }
-  printf("%d\n", mdc);
+  printf("%d", mdc);
 }
